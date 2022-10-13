a30fcd6e752cd9a12a91dd5d6c96c18acd70ed9c
ookami-kb <ookami.kb@gmail.com>
2022-10-13 19:30:07 +0200

    feat: lock portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
 
 import 'src/app/my_tmdb_app.dart';
 import 'src/di.dart';
 
-void main() {
+Future<void> main() async {
   configureDependencies(env: prod.name);
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MyTmdbApp());
 }
